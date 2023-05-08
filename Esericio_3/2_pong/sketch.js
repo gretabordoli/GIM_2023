938d70dc832e57c83acfbc447f0459756cdbda04
gretabordoli <gretabordoli@gmail.com>
2023-05-08 14:46:53 +0200

    commit

--- Esericio_3/2_pong/sketch.js
+++ Esericio_3/2_pong/sketch.js
@@ -1,16 +1,21 @@
-let posX = 200 
-let posY = 200 
-let velX = 5
-let velY = 8
+let posX   //200 è il punto centrale posso scrivere anche: 
+let posY 
+let velX
+let velY
 
 function setup(){
 
-	createCanvas(400, 400)
+	createCanvas(800, 400)
+	posX = width/2
+	posY = height/2
+    velX = random (-4, 4)
+    velY = random (-4, 4)
+	background (240, 60, 0)
 }
 
 function draw(){
 
- //background (200)
+ 
 
 ellipse ( posX, posY, 20, 20)
 posX = posX + velX
@@ -22,5 +27,9 @@ posY = posY + velY
 if (posX >= width|| posX <= 0) velX = -velX
 if (posY >= height|| posY <= 0) velY = -velY
 
-
 }
+
+ function keyPressed (){
+	save ("pong.png")
+}
+
